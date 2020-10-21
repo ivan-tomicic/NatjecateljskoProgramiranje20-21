129ad4df9aadc4d5c1177dc44775d541b29283b6
Ivan Tomicic <ivan.tomicic@fer.hr>
2020-10-21 23:23:49 +0200

    Added solutions for dz02

--- src/DZ02/Solutions/SortedAdjacentDifferences.java
+++ src/DZ02/Solutions/SortedAdjacentDifferences.java
@@ -1,4 +1,39 @@
 package DZ02.Solutions;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class SortedAdjacentDifferences {
+
+    static int N;
+    static ArrayList<Integer> array;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            int A = sc.nextInt();
+            array = new ArrayList<>(A);
+            for(int j = 0; j < A; j++) {
+                array.add(sc.nextInt());
+            }
+            array.sort(Comparator.comparingInt(a -> a));
+
+            int leftIndex = A/2 - 1;
+            int rightIndex = A/2;
+
+
+            while(leftIndex >= 0 && rightIndex <= A - 1) {
+                System.out.print(array.get(rightIndex) + " " + array.get(leftIndex) + " ");
+                rightIndex++;
+                leftIndex--;
+            }
+            if(rightIndex == A - 1) {
+                System.out.print(array.get(A - 1) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
